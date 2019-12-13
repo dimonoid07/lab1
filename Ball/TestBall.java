72566fd0ddb1542d48cce06e9da8bf6e99cf3e91
dimonoid07 <55837857+dimonoid07@users.noreply.github.com>
2019-12-13 23:53:07 +0300

    Update TestBall.java

--- Ball/TestBall.java
+++ Ball/TestBall.java
@@ -1,11 +1,11 @@
 package Ball;
 public class TestBall {
     public static void main(String[] args) {
-        Ball b1 = new Ball( "black", "regbi");
-        Ball b2 = new Ball( "pink", "football");
-        Ball b3 = new Ball( "grey", "basketball");
+        Ball b1 = new Ball( "blue", "football");
+        Ball b2 = new Ball( "green", "tennis");
+        Ball b3 = new Ball( "brown", "basketball");
 
-        b3.setColor("white");
+        b3.setColor("black");
 
         System.out.println(b1);
         System.out.println(b2);
